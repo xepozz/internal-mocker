4f6b7782e0ccab60a965b19336c01be14569ac86
Dmitrii Derepko <xepozz@list.ru>
2022-04-15 07:20:10 +0300

    Add tests

--- tests/Integration/DateTimeTest.php
+++ tests/Integration/DateTimeTest.php
@@ -4,10 +4,32 @@ declare(strict_types=1);
 namespace Xepozz\InternalFunctionMocker\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
+use Xepozz\InternalFunctionMocker\MockerState;
 
-class DateTimeTest extends TestCase
+final class DateTimeTest extends TestCase
 {
     public function testRun()
+    {
+        $obj = new DateTime();
+
+        $this->assertEquals(555, $obj->run());
+    }
+
+    public function testRun2()
+    {
+        $obj = new DateTime();
+
+        MockerState::addCondition(
+            'Xepozz\InternalFunctionMocker\Tests\Integration',
+            'time',
+            [],
+            100
+        );
+
+        $this->assertEquals(100, $obj->run());
+    }
+
+    public function testRun3()
     {
         $obj = new DateTime();
 
